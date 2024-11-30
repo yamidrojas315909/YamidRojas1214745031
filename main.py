7605f8432158ba0a357ccc44adc9e8f79225cc24
jonathansanchez2948 <jonathansanchez2948@correo.itm.edu.co>
2024-11-30 10:19:24 -0500

    Update main.py

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
 def main():
     print("Entrega 5 de la materia: Aplicaciones y servicios WEB. \nModificación 1 archivo main. \nNueva línea agregada. Modificación 2. \nNueva línea. Modificación 3 al archivo.")
+    print("Excelente trabajo")
  
 if __name__ == "__main__":
     main()
