8e8aabd5cd6cf80e1ecc2c0bca7c0caec62af1c7
yamidrojas315909 <yamidrojas315909@correo.itm.edu.co>
2024-11-29 21:46:21 -0500

    Modificación 4 al main.py
    Corrección a la Modificación 3.

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
 def main():
-    print("Entrega 5 de la materia: Aplicaciones y servicios WEB. \nModificación 1 archivo main. \nNueva línea agregada. Modificación 2. \Nueva línea. Modificación 3 al archivo.")
+    print("Entrega 5 de la materia: Aplicaciones y servicios WEB. \nModificación 1 archivo main. \nNueva línea agregada. Modificación 2. \nNueva línea. Modificación 3 al archivo.")
  
 if __name__ == "__main__":
     main()
